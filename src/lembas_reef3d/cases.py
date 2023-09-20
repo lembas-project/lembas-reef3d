752c3dbacc851dc5989d109606af5e353dff4305
mattkram <mkramer@anaconda.com>
2023-09-19 23:30:27 -0500

    Use jinja2 templating and add logic to make case run idempotent

--- src/lembas_reef3d/cases.py
+++ src/lembas_reef3d/cases.py
@@ -1,9 +1,9 @@
 import logging
-import shutil
 import subprocess
 from functools import cached_property
 from pathlib import Path
 
+from jinja2 import Environment, FileSystemLoader
 from lembas import Case, InputParameter, step
 from rich.logging import RichHandler
 
@@ -18,6 +18,7 @@ logger = logging.getLogger(__name__)
 
 
 TEMPLATE_DIR = Path.cwd().resolve() / "template"
+TEMPLATE_ENV = Environment(loader=FileSystemLoader(TEMPLATE_DIR))
 
 
 class Reef3dCase(Case):
@@ -31,15 +32,20 @@ class Reef3dCase(Case):
     def case_dir(self):
         return Path.cwd().resolve() / "cases" / "case_0"
 
-    @step
+    @step(condition=lambda self: not self.case_dir.exists())
     def create_case_dir_if_not_exists(self):
         self.log("Creating case directory: %s", self.case_dir)
         self.case_dir.mkdir(parents=True, exist_ok=True)
 
-    @step(requires="create_case_dir_if_not_exists")
+    @step(
+        requires="create_case_dir_if_not_exists",
+        condition=lambda self: not list(self.case_dir.glob("grid-*.dat")),
+    )
     def generate_mesh(self):
         self.log("Generating mesh using DIVEMesh")
         mesh_filename = "control.txt"
-        shutil.copyfile(TEMPLATE_DIR / mesh_filename, self.case_dir / mesh_filename)
+        template = TEMPLATE_ENV.get_template(mesh_filename)
+        with (self.case_dir / mesh_filename).open("w") as fp:
+            fp.write(template.render(num_processors=self.num_processors))
 
         subprocess.run(["divemesh"], cwd=str(self.case_dir))
