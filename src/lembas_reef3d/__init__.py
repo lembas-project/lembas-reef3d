11973067fbfb2279d3065f3b75a9ce3d37e9c126
mattkram <mkramer@anaconda.com>
2023-10-16 11:37:59 -0500

    Use setuptools and setuptools-scm

--- src/lembas_reef3d/__init__.py
+++ src/lembas_reef3d/__init__.py
@@ -1,4 +1,4 @@
-from lembas_reef3d.__about__ import __version__
+from lembas_reef3d._version import __version__
 from lembas_reef3d.cases import RegularWaveCase
 
 __all__ = ["RegularWaveCase", "__version__"]
