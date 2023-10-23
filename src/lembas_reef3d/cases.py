3e571c82d5980fff6b76b81996c06030a2e76089
mattkram <mkramer@anaconda.com>
2023-10-23 11:23:57 -0500

    Move generic post-processing functions to postprocessing.py

--- src/lembas_reef3d/cases.py
+++ src/lembas_reef3d/cases.py
@@ -1,10 +1,8 @@
-import re
 import subprocess
 from functools import cache
 from functools import cached_property
 from pathlib import Path
 
-import numpy as np
 import pandas as pd
 import xarray as xr
 from jinja2 import Environment
@@ -12,7 +10,9 @@ from jinja2 import FileSystemLoader
 from lembas import Case
 from lembas import InputParameter
 from lembas import step
-from lembas.logging import logger
+
+from lembas_reef3d.postprocessing import load_wave_elevation_line_probe
+from lembas_reef3d.postprocessing import load_wave_elevation_time_history
 
 MESH_FILENAME = "control.txt"
 CONTROL_FILENAME = "ctrl.txt"
@@ -97,10 +97,10 @@ class RegularWaveCase(Case):
                 self.results.wave_time_histories_theory = store.get("wave_time_histories_theory")
             except KeyError:
                 self.log("Processing wave elevation results")
-                self.results.wave_time_histories_simulation = _load_wave_elevation_time_history(
+                self.results.wave_time_histories_simulation = load_wave_elevation_time_history(
                     self.case_dir / "REEF3D_FNPF_WSF" / "REEF3D-FNPF-WSF-HG.dat"
                 )
-                self.results.wave_time_histories_theory = _load_wave_elevation_time_history(
+                self.results.wave_time_histories_theory = load_wave_elevation_time_history(
                     self.case_dir / "REEF3D_FNPF_WSF" / "REEF3D-FNPF-WSF-HG-THEORY.dat"
                 )
                 store.put("wave_time_histories_simulation", self.results.wave_time_histories_simulation)
@@ -115,7 +115,7 @@ class RegularWaveCase(Case):
             arr = xr.open_dataset(cdf_path)["elevation"]
         else:
             arr = xr.concat(
-                [_load_wave_elevation_line_probe(f) for f in sorted(line_probe_dir.glob("*.dat"))], dim="time"
+                [load_wave_elevation_line_probe(f) for f in sorted(line_probe_dir.glob("*.dat"))], dim="time"
             )
             arr.attrs["long_name"] = "free-surface elevation"
             arr.attrs["units"] = "m"
@@ -138,75 +138,3 @@ class RegularWaveCase(Case):
     def plot_line_probe_results(self):
         self.results.line_probe.isel(time=-1).plot.line(hue="y")
         plt().show()
-
-
-def _load_wave_elevation_time_history(file: Path) -> pd.DataFrame:
-    """Load the wave elevation time histories for a series of wave gauges from a file inside
-    the REEF3D_FNPF_WSF directory.
-
-    Returns a dataframe, where the index is the timestamp and each column is one wave probe."""
-    with file.open("r") as fp:
-        if m := re.match(r"number\s*of\s*gauges:\s*(\d+)", fp.readline()):
-            num_gauges = int(m.group(1))
-        else:
-            num_gauges = 0
-        logger.info(f"num gauges: {num_gauges}")
-
-        fp.readline()  # Blank line
-        fp.readline()  # Header: x_coord  y_coord
-        coord_data = [fp.readline().split() for _ in range(num_gauges)]
-
-        coord_df = (
-            pd.DataFrame(coord_data)
-            .rename(columns={0: "point", 1: "x_coord", 2: "y_coord"})
-            .astype("float64")
-            .astype({"point": "int32"})
-            .set_index("point")
-        )
-        _ = coord_df
-        # print(coord_df)
-
-        # Read rest of file as CSV
-        df = pd.read_csv(fp, delim_whitespace=True).set_index("time")
-
-        return df
-
-
-def _load_wave_elevation_line_probe(file: Path) -> xr.DataArray:
-    """Load the wave elevation results along line probes for a single timestep."""
-    with file.open("r") as fp:
-        if m := re.match(r"simtime:\s*(\w+)", fp.readline()):
-            sim_time = float(m.group(1))
-        else:
-            sim_time = 0
-
-        logger.debug(f"{sim_time=}")
-
-        if m := re.match(r"number\s*of\s*wsf-lines:\s*(\d+)", fp.readline()):
-            num_lines = int(m.group(1))
-        else:
-            num_lines = 0
-
-        logger.debug(f"{num_lines=}")
-
-        fp.readline()  # Blank line
-        fp.readline()  # Header: line_No, y_coord
-
-        # TODO: Load y_coords from file instead of hard-coding
-        y_coords = [0.005, np.inf]
-
-        for _ in range(num_lines):
-            fp.readline()
-        fp.readline()  # Wave Theory
-
-        # Read rest of file as CSV
-        fp.readline()
-        fp.readline()
-        fp.readline()
-        df = pd.read_csv(
-            fp, delim_whitespace=True, header=None, names=["X", *(f"P{i}" for i in range(1, num_lines + 1)), "W"]
-        ).set_index("X")
-
-        return xr.DataArray(df, coords={"x": df.index.to_numpy(), "y": y_coords}, dims=["x", "y"]).expand_dims(
-            {"time": [sim_time]}
-        )
---
+++ src/lembas_reef3d/postprocessing.py
@@ -0,0 +1,79 @@
+import re
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+from lembas.logging import logger
+
+
+def load_wave_elevation_time_history(file: Path) -> pd.DataFrame:
+    """Load the wave elevation time histories for a series of wave gauges from a file inside
+    the REEF3D_FNPF_WSF directory.
+
+    Returns a dataframe, where the index is the timestamp and each column is one wave probe."""
+    with file.open("r") as fp:
+        if m := re.match(r"number\s*of\s*gauges:\s*(\d+)", fp.readline()):
+            num_gauges = int(m.group(1))
+        else:
+            num_gauges = 0
+        logger.info(f"num gauges: {num_gauges}")
+
+        fp.readline()  # Blank line
+        fp.readline()  # Header: x_coord  y_coord
+        coord_data = [fp.readline().split() for _ in range(num_gauges)]
+
+        coord_df = (
+            pd.DataFrame(coord_data)
+            .rename(columns={0: "point", 1: "x_coord", 2: "y_coord"})
+            .astype("float64")
+            .astype({"point": "int32"})
+            .set_index("point")
+        )
+        _ = coord_df
+        # print(coord_df)
+
+        # Read rest of file as CSV
+        df = pd.read_csv(fp, delim_whitespace=True).set_index("time")
+
+        return df
+
+
+def load_wave_elevation_line_probe(file: Path) -> xr.DataArray:
+    """Load the wave elevation results along line probes for a single timestep."""
+    with file.open("r") as fp:
+        if m := re.match(r"simtime:\s*(\w+)", fp.readline()):
+            sim_time = float(m.group(1))
+        else:
+            sim_time = 0
+
+        logger.debug(f"{sim_time=}")
+
+        if m := re.match(r"number\s*of\s*wsf-lines:\s*(\d+)", fp.readline()):
+            num_lines = int(m.group(1))
+        else:
+            num_lines = 0
+
+        logger.debug(f"{num_lines=}")
+
+        fp.readline()  # Blank line
+        fp.readline()  # Header: line_No, y_coord
+
+        # TODO: Load y_coords from file instead of hard-coding
+        y_coords = [0.005, np.inf]
+
+        for _ in range(num_lines):
+            fp.readline()
+        fp.readline()  # Wave Theory
+
+        # Read rest of file as CSV
+        fp.readline()
+        fp.readline()
+        fp.readline()
+        df = pd.read_csv(
+            fp, delim_whitespace=True, header=None, names=["X", *(f"P{i}" for i in range(1, num_lines + 1)), "W"]
+        ).set_index("X")
+
+        return xr.DataArray(df, coords={"x": df.index.to_numpy(), "y": y_coords}, dims=["x", "y"]).expand_dims(
+            {"time": [sim_time]}
+        )
