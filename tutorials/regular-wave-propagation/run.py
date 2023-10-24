0bbd9398acaa627eb96851f56bc8e6d5350051e1
mattkram <mkramer@anaconda.com>
2023-10-23 23:05:44 -0500

    Expand case matrix

--- tutorials/regular-wave-propagation/run.py
+++ tutorials/regular-wave-propagation/run.py
@@ -5,8 +5,9 @@ from lembas_reef3d import RegularWaveCase
 case_list = CaseList()
 case_list.add_cases_by_parameter_sweep(
     RegularWaveCase,
-    num_processors=[4],
-    wave_height=[0.02],
-    wave_length=4.0,
+    num_processors=[4, 8],
+    wave_height=[0.02, 0.03, 0.04],
+    wave_length=[4.0, 6.0],
+    plot=True,
 )
 case_list.run_all()
